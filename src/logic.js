676f94986f2d824740b32870c32cc0f2100dc205
Maii0407 <87862368+Maii0407@users.noreply.github.com>
2022-01-09 21:29:07 +0800

    blocked mixed content fix

--- src/logic.js
+++ src/logic.js
@@ -2,7 +2,7 @@
 const logic = (function() {
     async function getWeather() {
         const userSearch = document.querySelector('.search-box');
-        const link = `http://api.openweathermap.org/data/2.5/weather?q=${userSearch.value}&units=metric&APPID=ee4fc729e17808280bb0635af6704ce1`;
+        const link = `https://api.openweathermap.org/data/2.5/weather?q=${userSearch.value}&units=metric&APPID=ee4fc729e17808280bb0635af6704ce1`;
 
         try {
             const response = await fetch( link, {mode: 'cors'} );
